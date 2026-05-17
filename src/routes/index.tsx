31e9f53d9a653af5c7e7f277f1be2238d011e1e7
Ehsan Bigzad <ehsanbigzad7@gmail.com>
2026-05-17 11:41:29 +0430

    refactor: remove RootLayout component and update import path in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { RootLayout } from "../layouts/root-layout";
+import { RootLayout } from "../components/layout";
 import { Home } from "../pages/home";
 
 export const router = createBrowserRouter([
